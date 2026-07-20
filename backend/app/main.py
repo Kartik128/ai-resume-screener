a8c2ae95927e67f782983c3789bba93c609b1185
Kartik Yadavalli <kartikyadavalli@Kartiks-MacBook-Air.local>
2026-07-20 20:55:26 +0530

    Allow all origins in CORSMiddleware for Cloud deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -84,14 +84,13 @@ app = FastAPI(
 )
 
 # CORS Configuration
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.BACKEND_CORS_ORIGINS,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register Exception Handlers
 app.add_exception_handler(AppException, app_exception_handler)
